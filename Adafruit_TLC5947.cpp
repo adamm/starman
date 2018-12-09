0c5a3ccc86572c92d0ec0e4481120be456386c52
Adam McDaniel <adam@array.org>
2018-12-08 22:42:26 -0700

    Build TLC5947 driver locally

--- Adafruit_TLC5947.cpp
+++ Adafruit_TLC5947.cpp
@@ -16,7 +16,7 @@
  ****************************************************/
 
 
-#include <Adafruit_TLC5947.h>
+#include "Adafruit_TLC5947.h"
 
 Adafruit_TLC5947::Adafruit_TLC5947(uint16_t n, uint8_t c, uint8_t d, uint8_t l) {
   numdrivers = n;
