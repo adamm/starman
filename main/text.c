1566289087d99f11903361270d72463c2992dfe6
Adam McDaniel <adam@array.org>
2022-10-30 15:11:18 -0600

    Starting framework for showing text on the display

---
+++ main/text.c
@@ -0,0 +1,9 @@
+#include <esp_log.h>
+#include "display.h"
+#include "font.h"
+
+#include "text.h"
+
+void text_draw_char(display_t* display, int16_t x, int16_t y, char chr) {
+    // Draw the character into the display at (x,y)
+}
---
+++ main/text.h
@@ -0,0 +1,10 @@
+#ifndef TEXT_H
+#define TEXT_H
+
+#include <string.h>
+
+#include "display.h"
+
+void text_draw_char(display_t*, int16_t, int16_t, char);
+
+#endif
